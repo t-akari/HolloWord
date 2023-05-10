06ad05a4b1da833d7f718bbaedf71dc5835d905e
akari <round.roko1g8g@gmail.com>
2023-05-10 22:49:03 +0900

    LocalDateTimeで時間の表示追加

--- src/main/java/com/example/helloworld/HelloWorldController.java
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -2,11 +2,20 @@ package com.example.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
 @RestController
 public class HelloWorldController {
 
+    @GetMapping("/time")
+    public LocalDateTime now() {
+        LocalDateTime now = LocalDateTime.now();
+        return now;
+    }
+
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() {
 
         return "hello world";
     }
